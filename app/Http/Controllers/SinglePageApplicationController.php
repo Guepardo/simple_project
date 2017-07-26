f54f591a0293d6a62a3621617f68655fdf454d19
Guepardo <bsinet@hotmail.com>
2017-07-26 18:18:38 -0300

    Melhorando identação e retirando whitespaces.

--- app/Http/Controllers/SinglePageApplicationController.php
+++ app/Http/Controllers/SinglePageApplicationController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class SinglePageApplicationController extends CrudController
 {
-    public function index(){
-        return view('layouts.app'); 
+    public function index() {
+        return view('layouts.app');
     }
 }
